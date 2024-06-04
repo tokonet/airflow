6e4f01ff3df8bea6a4cb769bdafee5072847a95b
tokonet <tokonet@naver.com>
2024-06-04 18:40:28 +0900

    op_argv add 2

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,7 +1,7 @@
 def get_sftp():
     print('SFTP 작업을 시작합니다.')
 
-def regist(name, sex, *argv):
+def regist(name, sex, *arg):
     print(f'성명 : {name}')
     print(f'성별 : {sex}')
     print(f'기타 : {argv}')
