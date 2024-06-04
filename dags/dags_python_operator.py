a3c05f279adcfbc0c8d9464df1636e7e153abd84
tokonet <tokonet@naver.com>
2024-06-04 15:45:59 +0900

    aaaaa

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -16,13 +16,13 @@ with DAG(
 #    params={"example_key": "example_value"},
 ) as dag:
     def select_fruit():
-    fruit = ['APPLE','BANANA','ORANGE','AVOCADO']
-    rand_int = random.randint(0,3)
-    print(fruit[rand_int])
+        fruit = ['APPLE','BANANA','ORANGE','AVOCADO']
+        rand_int = random.randint(0,3)
+        print(fruit[rand_int])
 
     py_t1 = PythonOperator(
         task_id="py_t1",
         python_callable=select_fruit
     )
-    
+
 py_t1
