23330c71da44e805bdc19b8787e430de6be0864b
tokonet <tokonet@naver.com>
2024-07-04 13:49:39 +0900

    aaaa

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -12,7 +12,7 @@ with DAG(
 ) as dag:
     bash_t1 = BashOperator(
         task_id="bash_t1",
-        bash_command='echo "data_interval_end : {{data_interval_end}} "'
+        bash_command="echo data_interval_end : {{data_interval_end}} "
     )
 
     bash_t2 = BashOperator(
@@ -21,7 +21,7 @@ with DAG(
             'START_DATE':'{{data_interval_start | ds}}',
             'END_DATE ':'{{data-interval_end | ds}}'
         }
-        bash_command='echo $START_DATE && echo $END_DATE'
+        bash_command="echo $START_DATE && echo $END_DATE"
     )
 
 bash_t1 >> bash_t2
