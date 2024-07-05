9f7b10336632da57c545da5786ca456c13e0cc94
tokonet <tokonet@naver.com>
2024-07-05 14:01:37 +0900

    aaaaa

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -5,9 +5,9 @@ from airflow import DAG
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_python_show_templates",
-    schedule="30 9 * * *",
-    start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
+    dag_id='dags_python_show_templates',
+    schedule='30 9 * * *',
+    start_date=pendulum.datetime(2024, 7, 1, tz="Asia/Seoul"),
     catchup=True,   # False : 시작일자(2023.03.10)의 누락된 구간을 돌리지 않는다. True인 경우 전체를 돌린다.
 ) as dag:
 
