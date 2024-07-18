6af38f9c0ac2840ad0eabc3680e282ea18f3a240
tokonet <tokonet@naver.com>
2024-07-18 15:40:52 +0900

    dsfsdfsf

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -51,6 +51,7 @@ class SeoulApiToCsvOperator(BaseOperator):
         request_url = f'{base_url}/{start_row}/{end_row}/'
         if self.base_dt is not None:
             request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
+        print('request_url : ',request_url)
         response = requests.get(request_url, headers)
         contents = json.loads(response.text)
 
