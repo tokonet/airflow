1ccc25b4a4aad314a3ac0b896acbb9c185ee8154
tokonet <tokonet@naver.com>
2024-07-19 09:18:11 +0900

    dsfsdfsf

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -37,9 +37,11 @@ class SeoulApiToCsvOperator(BaseOperator):
                 start_row = end_row + 1
                 end_row += 1000
 
-        print('self.path & filename :', self.path + '/' + self.file_name)
+        print('self.path :', self.path)
+        print('self.filename :', self.file_name)
         print('os.path.exists :', os.path.exists(self.path))
         if not os.path.exists(self.path):
+            print('mkdir -p {self.path}')
             os.system(f'mkdir -p {self.path}')
         print('total_row_df :', total_row_df)
         total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
