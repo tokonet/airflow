8ccec2ea4d29b7eb6e2b0284a097bd9ae2e084ab
tokonet <tokonet@naver.com>
2024-07-23 16:56:32 +0900

    ejnrovjeovjo

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -15,7 +15,7 @@ with DAG(
     tb_corona19_count_status = SeoulApiToCsvOperator(
         task_id = 'tb_corona19_count_status',
         dataset_nm = 'TbCorona19CountStatus',
-        path = '/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        path = '/opt/airflow/file/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name = 'TbCorona19CountStatus.csv'
     )
 
@@ -23,7 +23,7 @@ with DAG(
     tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
         task_id = 'tv_corona19_vaccine_stat_new',
         dataset_nm = 'tvCorona19VaccinestatNew',
-        path = '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        path = '/opt/airflow/file/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name = 'tvCorona19VaccinestatNew.csv'
     )
 
