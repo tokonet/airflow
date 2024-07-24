0971146a12aa0f756346beac27eb1d6202c7ef1f
tokonet <tokonet@naver.com>
2024-07-24 09:51:23 +0900

    fonocne

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -65,6 +65,8 @@ class SeoulApiToCsvOperator(BaseOperator):
 
         key_nm = list(contents.keys())[0]
         row_data = contents.get(key_nm).get('row')
+        print("key_nm:",key_nm)
+        print("row_data:",row_data)
         row_df = pd.DataFrame(row_data)
 
         return row_df
