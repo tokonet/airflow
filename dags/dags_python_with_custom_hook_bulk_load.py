655d8de910c56c79b182e9e19a8aed17045aa463
tokonet <tokonet@naver.com>
2024-07-24 15:46:14 +0900

    fonocne

--- dags/dags_python_with_custom_hook_bulk_load.py
+++ dags/dags_python_with_custom_hook_bulk_load.py
@@ -18,5 +18,5 @@ with DAG(
         python_callable=insrt_postgres,
         op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
                    'tbl_nm':'TbCorona19CountStatus_bulk2',
-                   'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'}
+                   'file_nm':'/opt/airflow/file/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'}
     )
