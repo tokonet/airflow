ac6eb42dac24e741ab9da97d498e6f4de42644f3
tokonet <tokonet@naver.com>
2024-07-24 10:03:54 +0900

    fonocne

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -43,6 +43,7 @@ class SeoulApiToCsvOperator(BaseOperator):
         if not os.path.exists(self.path):
             print(f'mkdir -p {self.path}')
             os.system(f'mkdir -p {self.path}')
+#        total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)  # 구분자 default ","
         total_row_df.to_csv(self.path + '/' + self.file_name, sep='\t', encoding='utf-8', index=False)
 
     def _call_api(self, base_url, start_row, end_row):
